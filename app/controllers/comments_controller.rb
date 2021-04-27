d4589133fd55f20ac8a4ea6c8b03eb40e87472d0
CelinaJackson <celinanjackson@gmail.com>
2021-04-27 15:01:34 -0400

    creating methods in controller and working on comments pages

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -20,27 +20,27 @@ class CommentsController < ApplicationController
         end 
     end 
 
-    def edit 
-        @comment = Comment.find(params[:id])
-    end 
+    # def edit 
+    #     @comment = Comment.find(params[:id])
+    # end 
 
-    def update 
-        if @comment.update(comment_params)
-            redirect_to comment_path(@comment)
-        else 
-            render :edit 
-        end 
-    end 
+    # def update 
+    #     if @comment.update(comment_params)
+    #         redirect_to comment_path(@comment)
+    #     else 
+    #         render :edit 
+    #     end 
+    # end 
 
-    def destroy 
-        if @comment.destroy
-            redirect_to user_path(current_user)
-            flash[:message] = "Your comment was deleted."
-        else 
-            flash[:message] = "You can't delete other user's comments."
-            redirect_to trip_path(@comment)
-        end 
-    end 
+    # def destroy 
+    #     if @comment.destroy
+    #         redirect_to user_path(current_user)
+    #         flash[:message] = "Your comment was deleted."
+    #     else 
+    #         flash[:message] = "You can't delete other user's comments."
+    #         redirect_to trip_path(@comment)
+    #     end 
+    # end 
 
     private 
 
