b1c600e14e76b7ed9d867c76f93928dc790fdcde
CelinaJackson <celinanjackson@gmail.com>
2021-04-26 21:59:29 -0400

    created sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,18 @@
+class SessionsController < ApplicationController 
+    def new 
+    end 
+
+    def omniauth 
+    end 
+
+    def create 
+    end 
+
+    def destroy 
+    end 
+
+    private 
+
+    def auth 
+    end 
+end 
