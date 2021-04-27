ed8d7bb26b2c61cbc6f4b9c3abe91c5f5b8215b0
CelinaJackson <celinanjackson@gmail.com>
2021-04-26 22:05:16 -0400

    updated routes to include static and sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :users, :comments, :trips, :locations 
+  resources :static, :users, :sessions, :trips, :locations, :comments
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
