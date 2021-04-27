e6c16a0c84853c49bad360c334d2121f513af518
CelinaJackson <celinanjackson@gmail.com>
2021-04-27 13:49:44 -0400

    basic comments_controller methods and installed flash gem

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,28 +1,51 @@
 class CommentsController < ApplicationController 
     def index 
+        @comments = Comment.all
     end 
 
     def show 
+        @comment = Comment.find(params[:id])
     end 
 
     def new 
+        @comment = Comment.new
     end 
     
     def create 
+        if @comment.valid? 
+            @comment.save
+            redirect_to comment_path(@comment)
+        else 
+            render :new 
+        end 
     end 
 
     def edit 
+        @comment = Comment.find(params[:id])
     end 
 
     def update 
+        if @comment.update(comment_params)
+            redirect_to comment_path(@comment)
+        else 
+            render :edit 
+        end 
     end 
 
     def destroy 
+        if @comment.destroy
+            redirect_to user_path(current_user)
+            flash[:message] = "Your comment was deleted."
+        else 
+            flash[:message] = "You can't delete other user's comments."
+            redirect_to trip_path(@comment)
+        end 
     end 
 
     private 
 
     def comment_params 
+        params.require(:comment).permit(:content)
     end 
 
 end 
