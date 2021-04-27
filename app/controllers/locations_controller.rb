2f114932e06e067b091362ff0d20d77e79ec0cf2
CelinaJackson <celinanjackson@gmail.com>
2021-04-26 21:45:15 -0400

    created controllers

---
+++ app/controllers/locations_controller.rb
@@ -0,0 +1,27 @@
+class LocationsController < ApplicationController
+    def index 
+    end 
+
+    def show 
+    end 
+
+    def new 
+    end 
+
+    def create 
+    end 
+
+    def edit 
+    end 
+
+    def update 
+    end 
+
+    def destroy 
+    end 
+
+    private 
+    
+    def location_params 
+    end 
+end 
---
+++ app/controllers/trips_controller.rb
@@ -0,0 +1,28 @@
+class TripsController < ApplicationController 
+    def index 
+    end 
+
+    def show 
+    end 
+
+    def new 
+    end 
+
+    def create 
+    end 
+
+    def edit 
+    end 
+    
+    def update 
+    end 
+
+    def destroy 
+    end 
+
+    private 
+
+    def trip_params 
+    end 
+
+end 
