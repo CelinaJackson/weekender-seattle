fa270d869c577cfd6e93697f5daf52690cd8d66c
CelinaJackson <celinanjackson@gmail.com>
2021-04-27 15:02:13 -0400

    creating methods in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,27 +1,30 @@
 class UsersController < ApplicationController 
     def index 
+        @users = User.all
     end 
 
     def show 
+        @user = User.find_by(params[:id])
+        redirect_to user_trips_path(@user)
     end 
 
     def new 
+        @user = User.new
     end 
 
     def create 
-    end 
-
-    def edit 
-    end 
-
-    def update 
-    end 
-
-    def destroy 
+        @user = User.new(user_params)
+        if @user.save
+           session[:user_id] = @user.id
+           redirect_to user_trip_path(@user)
+        else 
+            render :new 
+        end
     end 
 
     private 
 
     def user_params 
+        params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
     end 
 end 
