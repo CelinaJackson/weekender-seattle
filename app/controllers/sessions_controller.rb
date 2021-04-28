24ffe878122d9f2f075203cd7fe89047a62f6128
CelinaJackson <celinanjackson@gmail.com>
2021-04-28 17:39:19 -0400

    wrote methods in sessions controller and made new sessions form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,13 +2,23 @@ class SessionsController < ApplicationController
     def new 
     end 
 
+    def create 
+        @user = User.find_by(email: params[:email])
+        if @user && @user.authenticate(params[:password])
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        else 
+            flash[:alert] = "Email or Password you entered was invalid."
+            redirect_to new_session_path
+        end 
+    end 
+
     def omniauth 
     end 
 
-    def create 
-    end 
-
     def destroy 
+        session.delete :user_id
+        redirect_to root_path 
     end 
 
     private 
