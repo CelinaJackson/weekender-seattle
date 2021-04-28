9691aafde9068e72616b2eb55d2de6e336ae5d30
CelinaJackson <celinanjackson@gmail.com>
2021-04-28 16:10:49 -0400

    changed params

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,20 +1,35 @@
 class TripsController < ApplicationController 
     def index 
+        @trips = Trip.all
     end 
 
-    def show 
+    def show
+        @trip = Trip.find_by(params[:id]) 
     end 
 
     def new 
+        @trip = Trip.new(trip_params)
     end 
 
     def create 
+        if @trip.valid? 
+            @trip.save 
+            redirect_to trip_path(@trip)
+        else 
+            render :new 
+        end 
     end 
 
     def edit 
+        @trip = Trip.find_by(params[:id])
     end 
     
     def update 
+        if @trip.update(trip_params)
+            redirect_to trip_path(@trip)
+        else 
+            render :edit 
+        end
     end 
 
     def destroy 
@@ -23,6 +38,7 @@ class TripsController < ApplicationController
     private 
 
     def trip_params 
+        params.permit(:title, :content, location_attributes: [:city, :state, :zipcode])
     end 
 
 end 
